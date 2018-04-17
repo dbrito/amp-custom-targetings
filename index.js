2e9ae59cead94636aba45c3bfccbff09bc8fba09
dbrito <dbrito@uolinc.com>
2018-04-17 12:12:54 -0300

    Adicionando os headers a request

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/json', function (request, response) {
-    response.setHeader('Content-Type', 'application/json');
-    response.send(JSON.stringify({ meuk: 'raiz' }));
-});
 
-app.get('/json', function (request, response) {
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('AMP-Access-Control-Allow-Source-Origin', 'https://esporte.uol.com.br');
+    response.setHeader('Access-Control-Expose-Headers', 'AMP-Access-Control-Allow-Source-Origin');
     response.setHeader('Content-Type', 'application/json');
-    response.send(JSON.stringify({ meuk: 'json' }));
+    response.send(JSON.stringify({"targeting": {"time":["palmeiras","real-madrid"]}}));
 });
 
 app.listen(app.get('port'), function () {

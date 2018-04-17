2f5e9c5b17d5f3dabee9a048ff5ca3babf2b4942
dbrito <dbrito@uolinc.com>
2018-04-17 11:25:58 -0300

    corrigindo a porta da aplicação

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/json', function (request, response) {
     response.setHeader('Content-Type', 'application/json');
-    response.send(JSON.stringify({ meuk: 'quem liga' }));
+    response.send(JSON.stringify({ meuk: 'raiz' }));
 });
 
-app.listen(3000, function () {
-  console.log('Rodando a bagaça');
+app.get('/json', function (request, response) {
+    response.setHeader('Content-Type', 'application/json');
+    response.send(JSON.stringify({ meuk: 'json' }));
+});
+
+app.listen(app.get('port'), function () {
+  console.log('Rodando a bagaça', app.get('port'));
 });

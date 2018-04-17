0a710ae05d5e8b7e16c6ec19f297e151857c6144
dbrito <dbrito@uolinc.com>
2018-04-17 16:20:02 -0300

    headers

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/json', function (request, response) {
     response.setHeader('AMP-Access-Control-Allow-Source-Origin', 'https://esporte.uol.com.br');
     response.setHeader('Access-Control-Expose-Headers', 'AMP-Access-Control-Allow-Source-Origin');
     response.setHeader('Content-Type', 'application/json');
-    response.send(JSON.stringify({"targeting": {"time":["palmeiras","real-madrid"]}}));
+    response.send(JSON.stringify({"targeting": {"time":"palmeiras"}}));
 });
 
 app.listen(app.get('port'), function () {

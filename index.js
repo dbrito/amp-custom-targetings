e3e9f55844d0f9c4d2c795836550dc25c66fa106
dbrito <dbrito@uolinc.com>
2018-04-17 16:12:18 -0300

    headers

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000));
 
 app.get('/json', function (request, response) {
 
-    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Origin', 'https://esporte.uol.com.br');
     response.setHeader('AMP-Access-Control-Allow-Source-Origin', 'https://esporte.uol.com.br');
     response.setHeader('Access-Control-Expose-Headers', 'AMP-Access-Control-Allow-Source-Origin');
     response.setHeader('Content-Type', 'application/json');
